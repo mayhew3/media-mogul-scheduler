05a48676c774c8b05f26fa372f8af6ca5caa6e3e
Mayhew Seavey <commander.boffo@gmail.com>
2019-03-29 17:55:51 -0700

    Add SeriesDenormUpdater to multi-project project

--- task-scheduler/src/main/java/com/mayhew3/mediamogulscheduler/TaskScheduler.java
+++ task-scheduler/src/main/java/com/mayhew3/mediamogulscheduler/TaskScheduler.java
@@ -1,7 +1,21 @@
 package com.mayhew3.mediamogulscheduler;
 
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class TaskScheduler {
-  public static void main(String[] args)  {
-    System.out.println("Scheduler!");
+
+  public static void main(String[] args) {
+    System.out.println("Starting message!");
+
+    SeriesDenormUpdater seriesDenormUpdater = new SeriesDenormUpdater();
+
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(RabbitConfiguration.class);
+    AmqpTemplate amqpTemplate = context.getBean(AmqpTemplate.class);
+    amqpTemplate.convertAndSend(seriesDenormUpdater);
+
+    System.out.println("Sent to RabbitMQ: " + seriesDenormUpdater);
+
+    System.exit(0);
   }
 }
