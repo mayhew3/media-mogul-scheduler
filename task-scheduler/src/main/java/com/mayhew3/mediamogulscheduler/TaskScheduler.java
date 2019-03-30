66fcbb22a10fda441ad56bac0414983e6a83b4db
mayhew3 <commander.boffo@gmail.com>
2019-03-29 23:37:01 -0700

    Add Quartz Scheduler to run denorm updater

--- task-scheduler/src/main/java/com/mayhew3/mediamogulscheduler/TaskScheduler.java
+++ task-scheduler/src/main/java/com/mayhew3/mediamogulscheduler/TaskScheduler.java
@@ -1,21 +1,49 @@
 package com.mayhew3.mediamogulscheduler;
 
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.repeatMinutelyForever;
+import static org.quartz.TriggerBuilder.newTrigger;
+
 public class TaskScheduler {
 
-  public static void main(String[] args) {
-    System.out.println("Starting message!");
+  private final static Logger logger = LoggerFactory.getLogger(TaskScheduler.class);
 
-    SeriesDenormUpdater seriesDenormUpdater = new SeriesDenormUpdater();
+  private static AmqpTemplate amqpTemplate;
+
+  public static void main(String[] args) throws Exception {
+    logger.info("Starting message!");
 
     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(RabbitConfiguration.class);
-    AmqpTemplate amqpTemplate = context.getBean(AmqpTemplate.class);
-    amqpTemplate.convertAndSend(seriesDenormUpdater);
+    amqpTemplate = context.getBean(AmqpTemplate.class);
 
-    System.out.println("Sent to RabbitMQ: " + seriesDenormUpdater);
+    Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+    scheduler.start();
 
-    System.exit(0);
+    JobDetail jobDetail = newJob(SendUpdaterJob.class).build();
+
+    Trigger trigger = newTrigger()
+        .startNow()
+        .withSchedule(repeatMinutelyForever(5))
+        .build();
+
+    scheduler.scheduleJob(jobDetail, trigger);
+  }
+
+  public static class SendUpdaterJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) {
+      SeriesDenormUpdater updater = new SeriesDenormUpdater();
+
+      amqpTemplate.convertAndSend(updater);
+      logger.info("Sent to RabbitMQ: " + updater);
+    }
   }
 }
